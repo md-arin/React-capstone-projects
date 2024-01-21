e55aaa90c3a570c64c80d87142fc776b05b7ee9a
Md Arin Alam <alamlew@gmail.com>
2024-01-21 16:45:40 +0530

    git-fetcher

--- github-profile-fetcher/src/App.jsx
+++ github-profile-fetcher/src/App.jsx
@@ -1,17 +1,42 @@
 import ProfileCard from './Components/ProfileCard'
+import  axios  from 'axios'
 import './App.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [username, setUsername] = useState("")
+  const [hide,setHide] = useState(false);
+  const [username, setUsername] = useState("");
+  const [dp, setDp] = useState('');
+  const [name, setName] = useState("");
+  const [bio, setBio] = useState("");
+  const [followers, setFollowers] = useState("")
+  const [following, setFollowing] = useState("")
+  const [location, setLocation] = useState("")
+
+  useEffect(()=>{
+     getData();
+  },[username])
+
+  const getData = async()=>{
+    const res = await axios.get("https://api.github.com/users/"+username)
+    console.log(res.data)
+    setDp(res.data.avatar_url)
+    setName(res.data.name);
+    setBio(res.data.bio)
+    setFollowers(res.data.followers)
+    setFollowing(res.data.following)
+    setLocation(res.data.location)
+  }
 
   return (
     <>
     <div className='hero' >
         <input type="text" placeholder='Enter your github Username to fetch data' autoFocus onChange={(e) => setUsername(e.target.value)}/>
-        <button>Fetch</button>
+        <button onClick={()=>{
+          setHide(true);
+        }}>Fetch</button>
     </div>
-      <ProfileCard />
+      {hide ? <ProfileCard photo={dp} name={name} bio={bio} followers={followers} following={following} location={location}/> : null}
     </>
   )
 }
--- github-profile-fetcher/src/Components/ProfileCard.jsx
+++ github-profile-fetcher/src/Components/ProfileCard.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import react from 'react';
 
-function ProfileCard() {
-  return (
-    <div>ProfileCard</div>
-  )
+export default function ProfileCard({
+      photo,
+      name,
+      bio,
+      followers,
+      following,
+      location
+  }){
+    
+    return(
+      <div className=' flex'>
+        <div>
+          <img className=' rounded-3xl w-48' src={photo} alt="" />
+        </div>
+      
+        <div className=' px-4'>
+          <p><b>Name: </b> {name}</p>
+          <p>{bio}</p>
+          <div> <p><b>Followers :</b> {followers}</p>  <b>Following :</b> {following}</div>
+          <p>location : {location}</p>
+        </div>
+      </div>
+    )
 }
-
-export default ProfileCard
